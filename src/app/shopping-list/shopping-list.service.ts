d4079f0ae1f59f18eedd75a0dc02820e4578ca36
donyino <donyino@fh.org>
2019-08-14 13:39:48 +0300

    Passing ingredients from recipes to the shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -16,6 +16,12 @@ export class ShoppingListService {
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
 
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        this.ingredients.push(...ingredients);
+
         this.ingredientsChanged.emit(this.ingredients.slice())
     }
 }
