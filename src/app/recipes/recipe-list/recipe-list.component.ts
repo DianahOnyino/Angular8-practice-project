296052c752385a2f4a8fb5311d1cec903ad4a1de
donyino <donyino@fh.org>
2019-08-14 11:31:14 +0300

    Managing recipes in a recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,21 +11,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Githeri Recipe',
-      'Make sweet githeri',
-      'http://3.bp.blogspot.com/-gJ3x22_tPKY/VYkUAAA5CcI/AAAAAAAAAI0/bKVm0vTLqD8/s1600/githeri.jpg'),
-    new Recipe(
-      'Pilau Recipe',
-      'Sweet for classy visitors',
-      'https://img-global.cpcdn.com/001_recipes/b83d1b7fcea35412/640x640sq70/photo.jpg'
-    )
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,20 @@
-export class Recipe {
-    
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Githeri Recipe',
+            'Make sweet githeri',
+            'http://3.bp.blogspot.com/-gJ3x22_tPKY/VYkUAAA5CcI/AAAAAAAAAI0/bKVm0vTLqD8/s1600/githeri.jpg'),
+        new Recipe(
+            'Pilau Recipe',
+            'Sweet for classy visitors',
+            'https://img-global.cpcdn.com/001_recipes/b83d1b7fcea35412/640x640sq70/photo.jpg'
+        )
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
 }
